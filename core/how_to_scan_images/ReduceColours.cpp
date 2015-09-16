def5c06eab9d9d93960d95def22725c6d6b9f86a
Matthew Heard <matt.heard@oracle.com>
2015-09-17 08:13:34 +1200

    Start how_to_scan_images tutorial

---
+++ core/how_to_scan_images/ReduceColours.cpp
@@ -0,0 +1,27 @@
+// Copyright 2015 Matt Heard
+// http://mattheard.net
+// matt@mattheard.net
+// @mattheard
+
+#include <opencv2/opencv.hpp>
+#include <iostream>
+#include <string>
+
+int main(int argc, char **argv) {
+    using std::string;
+    const int expectedNumArgs = 2;
+    if (argc != expectedNumArgs) {
+        const string cmdName = "ConvertToGrey";
+        const string argsDesc = " <Image_Path>";
+        std::cout << "Usage: " << cmdName << argsDesc << std::endl;
+        return -1;
+    }
+    const string inputFilename = argv[1];
+    const cv::Mat srcImg = cv::imread(inputFilename);
+    if (!srcImg.data) {
+        const string err = "No image data";
+        std::cerr << err << std::cout;
+        return -1;
+    }
+    return 0;
+}
