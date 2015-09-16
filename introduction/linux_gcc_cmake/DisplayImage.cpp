46506aac9cd73164f1b286ebf110c60000440788
MattHeard <matt@mattheard.net>
2015-09-16 07:59:42 +0000

    Add usage info

--- introduction/linux_gcc_cmake/DisplayImage.cpp
+++ introduction/linux_gcc_cmake/DisplayImage.cpp
@@ -3,6 +3,17 @@
 // matt@mattheard.net
 // @mattheard
 
+#include <iostream>
+#include <string>
+
 int main(int argc, char **argv) {
+    const int expectedNumArgs = 2;
+    if (argc != expectedNumArgs) {
+        using std::string;
+        const string cmdName = "DisplayImage";
+        const string argsDesc = " <Image_Path>";
+        std::cout << "Usage: " << cmdName << argsDesc << std::endl;
+        return -1;
+    }
     return 0;
 }
