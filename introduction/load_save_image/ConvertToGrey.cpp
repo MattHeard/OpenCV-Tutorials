178ea805858fc9cdaa70503dca31eee467ff18e8
Matthew Heard <matt.heard@oracle.com>
2015-09-17 08:04:15 +1200

    Write grey img to file

--- introduction/load_save_image/ConvertToGrey.cpp
+++ introduction/load_save_image/ConvertToGrey.cpp
@@ -34,5 +34,6 @@ int main(int argc, char **argv) {
     }
     const Mat greyImg = convertToGrey(srcImg);
     const string outputFilename = "grey.jpg";
+    imwrite(outputFilename, greyImg);
     return 0;
 }
