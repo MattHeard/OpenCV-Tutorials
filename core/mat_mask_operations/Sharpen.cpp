49e37d811a9abd0bfd38291c443bae90af2ba1ec
Matthew Heard <matt.heard@oracle.com>
2015-09-18 16:14:51 +1200

    Start mat_mask_operations tutorial

---
+++ core/mat_mask_operations/Sharpen.cpp
@@ -0,0 +1,29 @@
+// Copyright 2015 Matt Heard
+// http://mattheard.net
+// matt@mattheard.net
+// @mattheard
+
+#include <opencv2/opencv.hpp>
+#include <iostream>
+#include <string>
+
+using cv::Mat;
+
+int main(int argc, char **argv) {
+    using std::string;
+    const int expectedNumArgs = 2;
+    if (argc != expectedNumArgs) {
+        const string cmdName = "ConvertToGrey";
+        const string argsDesc = " <Image_Path>";
+        std::cout << "Usage: " << cmdName << argsDesc << std::endl;
+        return -1;
+    }
+    const string inputFilename = argv[1];
+    const Mat srcImg = cv::imread(inputFilename);
+    if (!srcImg.data) {
+        const string err = "No image data";
+        std::cerr << err << std::endl;
+        return -1;
+    }
+    return 0;
+}
