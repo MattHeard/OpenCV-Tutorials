88c877f527ae5d931e58d50a41a7c72f946d7fb0
MattHeard <matt@mattheard.net>
2015-10-16 13:12:58 +0000

    Draw atom img

--- core/basic_geometric_drawing/Draw.cpp
+++ core/basic_geometric_drawing/Draw.cpp
@@ -9,16 +9,53 @@
 
 using cv::Mat;
 
+const int width = 400;
+
 Mat drawBlankCanvas() {
-    const int rows = 400;
-    const int cols = 400;
+    const int rows = width;
+    const int cols = width;
     const int type = CV_8UC3;
     return Mat::zeros(rows, cols, type);
 }
 
+void drawEllipse(Mat img, const double angle) {
+    const cv::Point center(width / 2, width / 2);
+    const cv::Size axes(width / 4, width / 16);
+    const double startAngle = 0;
+    const double endAngle = 360;
+    using cv::Scalar;
+    const Scalar BLUE(255, 0, 0);
+    const Scalar color = BLUE;
+    const int thickness = 2;
+    cv::ellipse(img, center, axes, angle, startAngle, endAngle, color,
+            thickness);
+}
+
+void drawAtomEllipses(Mat img) {
+    const double angles[] = { -45.0, 0.0, 45.0, 90.0 };
+    const size_t numAngles = sizeof(angles) / sizeof(angles[0]);
+    for (size_t i = 0; i < numAngles; ++i) {
+        const double angle = angles[i];
+        drawEllipse(img, angle);
+    }
+}
+
+void drawAtomDot(Mat img) {
+    const cv::Point center(width / 2, width / 2);
+    const int radius = width / 32;
+    using cv::Scalar;
+    const Scalar RED(0, 0, 255);
+    const Scalar color = RED;
+    const int FILL = -1;
+    const int thickness = FILL;
+    cv::circle(img, center, radius, color, thickness);
+}
+
 void drawAtom() {
-    Mat atomImg = drawBlankCanvas();
-    cv::imwrite("atom.jpg", atomImg);
+    Mat img = drawBlankCanvas();
+    drawAtomEllipses(img);
+    drawAtomDot(img);
+    cv::imwrite("atom.jpg", img);
 }
 
 int main(int argc, char **argv) {
